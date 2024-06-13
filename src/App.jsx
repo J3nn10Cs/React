e1ad790d68707ef7e84d9a47ac11af4d6832d643
Jennifer <jennifer15aries@gmail.con>
2024-06-12 22:57:44 -0500

    Actualizacion Api

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
+import { useEffect, useState } from "react"
+
 function App() {
+
+  //Lo iniciamos en un array vacío
+  const [criptos,setCriptos] = useState()
+
+  useEffect(() => {
+    fetch("https://api.coincap.io/v2/assets")
+    //La respuesta la convierte en Json
+    .then( (resp) => resp.json() )
+    .then((data) =>{
+      setCriptos(data.data)
+    } )      
+    .catch( () => {
+      console.error("La petición falló")
+    } )
+  },[])
+
+  //Si criptos aun no existe
+  if(!criptos)
+    return <span> Cargando ...</span>
 
   return (
     <>
-      <h1>Hola mundo</h1>
+      <h1>Lista Cripto Money</h1>
+      <ol>
+        { criptos.map( ({name,priceUsd}) => (
+          <li> Nombre : {name} / Precio : {priceUsd}</li>
+        )) }
+      </ol>
     </>
   )
 }
